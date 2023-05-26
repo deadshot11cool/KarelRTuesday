2fa0c15ecf024a3f189078784595ad113c5f96ce
Mark Lessard <nlessard@mon-avenir.ca>
2023-05-26 12:06:06 -0400

    question 1 pour la tache selective
    dans cette script tu peux voir que il y a deux action de move, mais seulment 1 fontionne. c'est a cause que Karel est commander de ne pas bouger s'il y a une mur.

---
+++ tasks/selective.rb
@@ -0,0 +1,19 @@
+#!/opt/local/bin/ruby
+#Copyright 2012 Joseph Bergin
+#License: Creative Commons Attribution-Noncommercial-Share Alike 3.0 United States License
+
+$graphical = true
+
+require_relative "../karel/robota"
+require_relative "../mixins/lettremix3"
+require_relative "../mixins/sensor_pack"
+require_relative "../karel/ur_robot"
+
+class Selective < UrRobot   
+    include SensorPack
+    def move
+        if front_is_clear? 
+            super
+        end
+    end
+end
