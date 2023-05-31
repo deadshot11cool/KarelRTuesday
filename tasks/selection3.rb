db0fbbbde2cd102655821db8d0f986b518c89566
Mark Lessard <nlessard@mon-avenir.ca>
2023-05-31 14:07:09 -0400

    question 3 de selection

---
+++ tasks/selection3.rb
@@ -0,0 +1,10 @@
+
+num1 = rand(0..20)
+num2 = rand(0..20)
+if num1 > num2
+	puts "Num1 = #{num1} et Num2 = #{num2}       num1 (#{num1}) est plus grande que num2 (#{num2})"
+else
+	puts "Num1 = #{num1} et Num2 = #{num2}       num2 #{num2} est plus grande que num1: (#{num1})"
+#elsif num1 = num2
+	#puts "ils sont egale"
+end
